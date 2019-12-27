e304f62ad702413880101e4107071e41a7839c8e
Karen V <karenvo@usc.edu>
2019-12-27 10:48:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-function wakeDog(name, breed) {
-  console.log(`Wake ${name} the ${breed}`)
+function wakeDog(a, b) {
+  console.log(`Wake ${a} the ${b}`)
 }
 
-function leashDog(name, breed) {
-  console.log(`Leash ${name} the ${breed}`)
+function leashDog(a, b) {
+  console.log(`Leash ${a} the ${b}`)
 }
 
-function walkToPark(name, breed) {
-  console.log(`Walk to the park with ${name} the ${breed}`)
+function walkToPark(a, b) {
+  console.log(`Walk to the park with ${a} the ${b}`)
 }
 
-function throwFrisbee(name, breed) {
-  console.log(`Throw the frisbee for ${name} the ${breed}`)
+function throwFrisbee(a, b) {
+  console.log(`Throw the frisbee for ${a} the ${b}`)
 }
 
-function walkHome(name, breed) {
-  console.log(`Walk home with ${name} the ${breed}`)
+function walkHome(a, b) {
+  console.log(`Walk home with ${a} the ${b}`)
 }
 
-function unleashDog(name, breed) {
-  console.log(`Unleash ${name} the ${breed}`)
+function unleashDog(a, b) {
+  console.log(`Unleash ${a} the ${b}`)
 }
