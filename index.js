ddd66b2d7023d3a33ad3c96d6213326456b59c77
Karen V <karenvo@usc.edu>
2019-12-27 10:38:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ function wakeDog() {
 }
 
 function leashDog() {
-
+  "Leash Boo Radley the Pibble"
 }
 
 function walkToPark() {
-
+  "Walk to the park with Bunny the Labrador"
 }
 
 function throwFrisbee() {
-
+  "Throw the frisbee for Mary J. Blige the Papillon"
 }
 
 function walkHome() {
-
+  "Walk home with Santa Paws the Husky"
 }
 
 function unleashDog() {
-
+  "Unleash Lauren the Golden Retriever"
 }
